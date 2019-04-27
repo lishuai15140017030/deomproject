1127e3bede892a5b5f21e06ad7f4e10aba533c68
GRANDSOFT\liujb-b <liujb-b@glodon.com>
2019-04-27 16:55:37 +0800

    初始化目录结构

---
+++ src/components/Layout.vue
@@ -0,0 +1,58 @@
+/*
+ * @Author: Chanh Wang
+ * @Date: 2018-11-02 18:51:30
+ * @Last Modified by: liujb-b
+ * @Last Modified time: 2019-04-27 16:28:03
+ */
+<template>
+  <div class="layout">
+    <div class="header">我是头部</div>
+    <div class="left-side"><left-side></left-side></div>
+    <div class="right-side"><router-view></router-view></div>
+  </div>
+</template>
+
+<script>
+import LeftSide from './LeftSide.vue'
+export default {
+  name: 'Layout',
+  components: {
+    LeftSide
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+  },
+  created () {
+  },
+  methods: {}
+
+}
+
+</script>
+<style lang="scss" scoped>
+.layout{
+  height: 100%;
+  position: relative;
+  .header{
+    height: 80px;
+    background: #000;
+  }
+  .left-side{
+    position: absolute;
+    left: 0;
+    top: 80px;
+    bottom: 0;
+    width: 280px;
+  }
+  .right-side{
+    position: absolute;
+    left: 280px;
+    top: 80px;
+    bottom: 0;
+    right: 0;
+  }
+}
+</style>
---
+++ src/components/LeftSide.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row class="tac">
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      router
+    >
+      <el-menu-item index="/index">
+        <i class="el-icon-menu"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+      <el-menu-item index="/page1">
+        <i class="el-icon-menu"></i>
+        <span slot="title">页面1</span>
+      </el-menu-item>
+      <el-menu-item index="/page2">
+        <i class="el-icon-document"></i>
+        <span slot="title">页面2</span>
+      </el-menu-item>
+    </el-menu>
+  </el-row>
+</template>
+<script>
+export default {
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tac{
+  height: 100%;
+  background-color: rgb(84, 92, 100);
+  overflow: auto;
+  .el-menu-vertical-demo{
+    height: 100%;
+  }
+}
+</style>
